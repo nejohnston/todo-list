f17d8db2f081a411b875f35e214d856913fc6e01
njohnstud <n.e.johnston@hotmail.com>
2017-05-06 23:46:25 -0700

    Added todos-list-item component to populate list

--- src/todos-list.js
+++ src/todos-list.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import TodosListHeader from './todos-list-header';
 import logo from './logo.svg';
 import './App.css';
 
 export default class TodosList extends React.Component {
+  renderItems() {
+    return _.map(this.props.todos, (todo, index) => <TodosListItem key={index} {...todo}/>)
+  }
+
   render() {
     console.log(this.props.todos);
 
     return (
       <table>
         <TodosListHeader />
+        <tr>
+          {this.renderItems()}
+        </tr>
       </table>
     );
   }
