012dcf54abff79103baa6ae3855af4b9d1b8729b
njohnstud <n.e.johnston@hotmail.com>
2017-05-26 22:36:29 -0700

    Spaced item list header.

--- src/todos-list-header.js
+++ src/todos-list-header.js
@@ -7,10 +7,14 @@ import './App.css';
 export default class TodosListHeader extends React.Component {
   render() {
     return (
+      <div className="todo-items-header">
               <div>
                   <h2>Task</h2>
+              </div>
+              <div>
                   <h2>Action</h2>
               </div>
+        </div>
     );
   }
 }
